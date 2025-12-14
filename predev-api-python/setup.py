5953cd075d5bfc3a111ab3ec2e5081d71a3e06ab
eip_achilles <arjun@pre.dev>
2025-12-14 16:37:00 -0500

    chore: bump version to 0.10.3

--- predev-api-python/setup.py
+++ predev-api-python/setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="predev-api",
-    version="0.10.2",
+    version="0.10.3",
     author="Pre.dev",
     author_email="support@pre.dev",
     description="Python client for the Pre.dev Architect API - Generate comprehensive software specifications",
